69dd99a14012958f911cf304c3d664ce8f94b57d
ruanxuanly <ruanxuanly@163.com>
2018-07-27 16:03:33 +0800

    add private file first

---
+++ app/src/main/java/com/icomm_semi/xuan/babystore/Key.java
@@ -0,0 +1,11 @@
+package com.icomm_semi.xuan.babystore;
+
+public class Key {
+    public static String MQTT_USER_NAME  =  "dut/sv6266";
+    public static String MQTT_SERVER  =  "dut/sv6266";
+    public static String MQTT_PWD  =  "dut/sv6266";
+
+    public static String IDA_APP_ID  =  "dut/sv6266";
+    public static String IDA_DEV_ID  =  "dut/sv6266";
+    public static String IDA_SRC_  =  "dut/sv6266";
+}
